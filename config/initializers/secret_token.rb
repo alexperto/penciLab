1bbc9f8e750f849f4cae0a2aeeb5262b4920fec0
Alex Cabrera <alexperto@gmail.com>
2014-01-14 21:40:32 -0800

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PenciLab::Application.config.secret_key_base = '898a948e03e4c808b5a0edc2ff00d4930f86931251a1dc0e2b36b13b896c3d9f4e8dab389d583da5b2dac0e4b9ed1c9aac64b34eb63216cc94fc0cf67e6a6f30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PenciLab::Application.config.session_store :cookie_store, key: '_penciLab_session'
