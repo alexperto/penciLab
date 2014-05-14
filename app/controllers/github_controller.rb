de89cab4d1585b3e82640329c244c61b573409bd
Alex Cabrera <alexperto@gmail.com>
2014-05-13 23:15:29 -0700

    Fix: ActionController::InvalidAuthenticityToken

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,4 +1,6 @@
 class GithubController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def webhook
     puts params.inspect
     render :json => { :status => 200, :response => 'OK' }
